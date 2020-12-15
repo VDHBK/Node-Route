ab65c552ec6c1ca772b66ded755171f4d0bb200d
ヴー ドゥック フィー <CXJ772@muratec.co.jp>
2020-12-15 10:53:14 +0900

    test

--- findRoute.js
+++ findRoute.js
@@ -29,7 +29,6 @@ const getPoints = (edges) => {
 
 const getInitPoints = (startPointName, points) => {
     let results = JSON.parse (JSON.stringify (points)); // clone
-   // console.log(points);
     results[startPointName].value = 0;
     return results;
 }
--- test/findRoute.test.js
+++ test/findRoute.test.js
@@ -1,14 +1,13 @@
 const findRoute = require("../findRoute.js");
 const assert = require('assert');
 const edges = findRoute.readEdges('expects.csv');
-//console.log(edges.length);
 const graph = findRoute.makeGraph(edges);
 const points   = findRoute.getPoints (edges);
 describe('test log value', () => {
     it('test log value', () => {
-     const result = findRoute.getShortestPath("22", "15", points, graph )
-     assert.equal(result.value, 138670);
-     assert.equal(result.path.join(" "), "22 69 49 50 51 55 56 15");
+     const result = findRoute.getShortestPath("132", "67", points, graph )
+     assert.equal(result.value, 143500);
+    assert.equal(result.path.join(" "), "132 89 136 74 76 72 79 49 66 67");
     });
  
 });
